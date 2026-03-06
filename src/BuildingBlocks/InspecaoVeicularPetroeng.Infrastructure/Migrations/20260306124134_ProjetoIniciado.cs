6afd661bc8e6eee911c403fe032d49748f5b2238
FillipeEduardo <fillipe_oliveira@outlook.com.br>
2026-03-06 09:47:53 -0300

    Cria migration inicial e tabela de usuários
    Adiciona a primeira migration do projeto com Entity Framework Core e PostgreSQL, criando a tabela "usuarios" com campos obrigatórios, restrições de tamanho e índice único para o email. Inclui arquivos de migration, designer e snapshot do contexto.

---
+++ src/BuildingBlocks/InspecaoVeicularPetroeng.Infrastructure/Migrations/20260306124134_ProjetoIniciado.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace InspecaoVeicularPetroeng.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProjetoIniciado : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "usuarios",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    email = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    senha = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    nome_completo = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    telefone = table.Column<string>(type: "character varying(11)", maxLength: 11, nullable: false),
+                    perfil = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_usuarios", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_usuarios_email",
+                table: "usuarios",
+                column: "email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "usuarios");
+        }
+    }
+}
