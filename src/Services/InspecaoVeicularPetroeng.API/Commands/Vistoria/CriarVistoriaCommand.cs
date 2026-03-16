91296753b93cbaedb674025924016a37a511f15f
FillipeEduardo <fillipe_oliveira@outlook.com.br>
2026-03-16 11:17:52 -0300

    Validação de itens e status ao criar vistoria
    Adiciona validações no handler CriarVistoriaCommandHandler para garantir que itens e status das inspeções existam no banco antes de criar uma vistoria. Retorna erro 400 caso algum não exista. Inclui novos namespaces necessários para as validações.

--- src/Services/InspecaoVeicularPetroeng.API/Commands/Vistoria/CriarVistoriaCommand.cs
+++ src/Services/InspecaoVeicularPetroeng.API/Commands/Vistoria/CriarVistoriaCommand.cs
@@ -1,7 +1,9 @@
-﻿using InspecaoVeicularPetroeng.Domain.Entities;
+﻿using System.Net;
+using InspecaoVeicularPetroeng.Domain.Entities;
 using InspecaoVeicularPetroeng.Domain.Results;
 using InspecaoVeicularPetroeng.Infrastructure.Data;
 using InspecaoVeicularPetroeng.Mediator.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace InspecaoVeicularPetroeng.API.Commands.Vistoria;
 
@@ -28,5 +30,34 @@ public class CriarVistoriaCommandHandler(AppDbContext context) : IRequestHandler
 {
     public async Task<Result> Handler(CriarVistoriaCommand request, CancellationToken cancellationToken)
     {
+        foreach (var inspecao in request.Inspecoes)
+        {
+            var existeItemDaInspecao = await context.Itens.AnyAsync(i => i.Id == inspecao.Item.Id, cancellationToken);
+            if (!existeItemDaInspecao)
+                return new ErrorResult(["Um dos itens da inspeção não existe."], HttpStatusCode.BadRequest);
+            inspecao.ItemId = inspecao.Item.Id;
+
+            var existeStatusDaInspecao =
+                await context.StatusInspecao.AnyAsync(st => st.Id == inspecao.Status.Id, cancellationToken);
+            if (!existeStatusDaInspecao)
+                return new ErrorResult(["Um dos status da inspeção não existe."], HttpStatusCode.BadRequest);
+            inspecao.StatusId = inspecao.Status.Id;
+        }
+
+        Domain.Entities.Vistoria vistoria = request;
+
+        await context.AddAsync(vistoria, cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
+
+        var result = new
+        {
+            vistoria.Id,
+            vistoria.Data,
+            request.Inspecoes,
+            vistoria.QuilometragemVeiculo,
+            vistoria.VeiculoId
+        };
+
+        return new SuccessResult("Vistoria criada com sucesso.", HttpStatusCode.Created);
     }
 }
